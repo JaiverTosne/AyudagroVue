b38ee8190ef45c3370ea84e72db5899dcc3dd1b0
Yennifer <yenifernataly2003@gmail.com>
2022-09-21 14:00:53 -0500

    login

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,7 @@
     <div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
+      <router-link to="/Login">Inicio</router-link>
     </div>
     <router-view/>
   </div>
--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
-
+import Login from '../views/LoginView.vue'
 Vue.use(VueRouter)
 
 const routes = [
@@ -10,6 +10,12 @@ const routes = [
     name: 'Home',
     component: Home
   },
+  {
+    path: '/Login',
+    name: 'Login',
+    component: () => import( '../views/LoginView.vue')
+  },
+
   {
     path: '/about',
     name: 'About',
